9897908f2f68a9edd402e42f4d9fecc8b7946ff6
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-26 20:25:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr)
+  batch_badge_arr = []
   arr.each do |name|
-    return "Hello, my name is #{name}."
+    batch_badge_arr.push("Hello, my name is #{name}.")
   end
+  return batch_badge_arr
 end
